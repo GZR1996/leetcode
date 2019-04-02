511356a41336c4addcb3186e36fe96ec3b76ed53
GZR1996 <676580542@qq.com>
2019-04-02 18:11:47 +0800

    35,36

--- src/leetcode/Solution.java
+++ src/leetcode/Solution.java
@@ -23,4 +23,36 @@ public class Solution {
 		}
 	}
 	
+	public static boolean isValidSudoku(char[][] board) {
+		for (int row = 0; row < 9; ++row) {
+			boolean[] rows = new boolean[9];
+			boolean[] cols = new boolean[9];
+			boolean[] cube = new boolean[9];
+			
+			for (int col = 0; col < 9; col++) {
+				int crow = 3 * (row / 3) + (col / 3);
+		        int ccol = 3 * (row % 3) + (col % 3);
+		        
+		        if (!isValid(rows, board[row][col]) 
+		        		|| !isValid(cols, board[col][row])
+		        		|| !isValid(cube, board[crow][ccol])) {
+					return false;
+				}
+			}
+		}
+		return true;
+	}
+	
+	private static boolean isValid(boolean[] valids, char target) {
+		if (target == '.') {
+			return true;
+		}
+		
+		int num = Character.getNumericValue(target) - 1;
+		if (valids[num]) {
+			return false;
+		} 
+		valids[num] = true;
+		return true;
+	}
 }
